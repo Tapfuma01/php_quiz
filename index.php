54e4e84d1f69a2d5bcc6655d8e52e1ee749171d1
Tapfuma <enosmhundwa@gmail.com>
2019-08-19 10:19:16 +0200

    update on the php

--- index.php
+++ index.php
@@ -4,19 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
-<body bgcolor='aqua'>
+<body bgcolor='white'>
     <h1>ENVIRONMENTAL QUIZ</h1>
     <br/>
     <br/>
     <br/>
 
 <form action="process.php" method="post">
+    <input type="submit">
     
 <ol>
      <li>
-         <h3> What is my name<h3>
+         <h3> What is my name?.<h3>
              <div>
              <input type="radio" name='firstAnswer' value='A' />
              <label for='firstAnswerA'>a)Tanaka</label>
@@ -40,8 +42,222 @@
 
     </li>
  
+    <li>
+         <h3> What is my favorite music?.<h3>
+             <div>
+             <input type="radio" name='firstAnswer' value='A' />
+             <label for='firstAnswerA'>a)Pop</label>
+            </div>
 
+            <div>
+             <input type="radio" name='firstAnswer' value='B' />
+             <label for='firstAnswerB'>b)Dancehall</label>
+            </div>
 
+            <div>
+             <input type="radio" name='firstAnswer' value='C' />
+             <label for='firstAnswerC'>c)Country</label>
+            </div>
+
+            <div>
+             <input type="radio" name='firstAnswer' value='D' />
+             <label for='firstAnswerD'>d)All of the above</label>
+            </div>
+                 
+
+    </li>
+    <li>
+         <h3> What is my favorite <h3>
+             <div>
+             <input type="radio" name='firstAnswer' value='A' />
+             <label for='firstAnswerA'>a)Tanaka</label>
+            </div>
+
+            <div>
+             <input type="radio" name='firstAnswer' value='B' />
+             <label for='firstAnswerB'>b)Tawnda</label>
+            </div>
+
+            <div>
+             <input type="radio" name='firstAnswer' value='C' />
+             <label for='firstAnswerC'>c)Tapfuma</label>
+            </div>
+
+            <div>
+             <input type="radio" name='firstAnswer' value='D' />
+             <label for='firstAnswerD'>d)Tafara</label>
+            </div>
+                 
+
+    </li>
+    <li>
+         <h3> What is my name<h3>
+             <div>
+             <input type="radio" name='firstAnswer' value='A' />
+             <label for='firstAnswerA'>a)Tanaka</label>
+            </div>
+
+            <div>
+             <input type="radio" name='firstAnswer' value='B' />
+             <label for='firstAnswerB'>b)Tawnda</label>
+            </div>
+
+            <div>
+             <input type="radio" name='firstAnswer' value='C' />
+             <label for='firstAnswerC'>c)Tapfuma</label>
+            </div>
+
+            <div>
+             <input type="radio" name='firstAnswer' value='D' />
+             <label for='firstAnswerD'>d)Tafara</label>
+            </div>
+                 
+
+    </li>
+    <li>
+         <h3> What is my name<h3>
+             <div>
+             <input type="radio" name='firstAnswer' value='A' />
+             <label for='firstAnswerA'>a)Tanaka</label>
+            </div>
+
+            <div>
+             <input type="radio" name='firstAnswer' value='B' />
+             <label for='firstAnswerB'>b)Tawnda</label>
+            </div>
+
+            <div>
+             <input type="radio" name='firstAnswer' value='C' />
+             <label for='firstAnswerC'>c)Tapfuma</label>
+            </div>
+
+            <div>
+             <input type="radio" name='firstAnswer' value='D' />
+             <label for='firstAnswerD'>d)Tafara</label>
+            </div>
+                 
+
+    </li>
+    <li>
+         <h3> What is my name<h3>
+             <div>
+             <input type="radio" name='firstAnswer' value='A' />
+             <label for='firstAnswerA'>a)Tanaka</label>
+            </div>
+
+            <div>
+             <input type="radio" name='firstAnswer' value='B' />
+             <label for='firstAnswerB'>b)Tawnda</label>
+            </div>
+
+            <div>
+             <input type="radio" name='firstAnswer' value='C' />
+             <label for='firstAnswerC'>c)Tapfuma</label>
+            </div>
+
+            <div>
+             <input type="radio" name='firstAnswer' value='D' />
+             <label for='firstAnswerD'>d)Tafara</label>
+            </div>
+                 
+
+    </li>
+    <li>
+         <h3> What is my name<h3>
+             <div>
+             <input type="radio" name='firstAnswer' value='A' />
+             <label for='firstAnswerA'>a)Tanaka</label>
+            </div>
+
+            <div>
+             <input type="radio" name='firstAnswer' value='B' />
+             <label for='firstAnswerB'>b)Tawnda</label>
+            </div>
+
+            <div>
+             <input type="radio" name='firstAnswer' value='C' />
+             <label for='firstAnswerC'>c)Tapfuma</label>
+            </div>
+
+            <div>
+             <input type="radio" name='firstAnswer' value='D' />
+             <label for='firstAnswerD'>d)Tafara</label>
+            </div>
+                 
+
+    </li>
+    <li>
+         <h3> What is my name<h3>
+             <div>
+             <input type="radio" name='firstAnswer' value='A' />
+             <label for='firstAnswerA'>a)Tanaka</label>
+            </div>
+
+            <div>
+             <input type="radio" name='firstAnswer' value='B' />
+             <label for='firstAnswerB'>b)Tawnda</label>
+            </div>
+
+            <div>
+             <input type="radio" name='firstAnswer' value='C' />
+             <label for='firstAnswerC'>c)Tapfuma</label>
+            </div>
+
+            <div>
+             <input type="radio" name='firstAnswer' value='D' />
+             <label for='firstAnswerD'>d)Tafara</label>
+            </div>
+                 
+
+    </li>
+    <li>
+         <h3> What is my name<h3>
+             <div>
+             <input type="radio" name='firstAnswer' value='A' />
+             <label for='firstAnswerA'>a)Tanaka</label>
+            </div>
+
+            <div>
+             <input type="radio" name='firstAnswer' value='B' />
+             <label for='firstAnswerB'>b)Tawnda</label>
+            </div>
+
+            <div>
+             <input type="radio" name='firstAnswer' value='C' />
+             <label for='firstAnswerC'>c)Tapfuma</label>
+            </div>
+
+            <div>
+             <input type="radio" name='firstAnswer' value='D' />
+             <label for='firstAnswerD'>d)Tafara</label>
+            </div>
+                 
+
+    </li>
+    <li>
+         <h3> What is my name<h3>
+             <div>
+             <input type="radio" name='firstAnswer' value='A' />
+             <label for='firstAnswerA'>a)Tanaka</label>
+            </div>
+
+            <div>
+             <input type="radio" name='firstAnswer' value='B' />
+             <label for='firstAnswerB'>b)Tawnda</label>
+            </div>
+
+            <div>
+             <input type="radio" name='firstAnswer' value='C' />
+             <label for='firstAnswerC'>c)Tapfuma</label>
+            </div>
+
+            <div>
+             <input type="radio" name='firstAnswer' value='D' />
+             <label for='firstAnswerD'>d)Tafara</label>
+            </div>
+                 
+
+    </li>
 </ol>
 
 
--- process.php
+++ process.php
@@ -4,10 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
-<body>
-    
+<body bgcolor='aqua'>
+    <h1>YOUR SCORE</h1>
+    <br/>
+    <br/>
+    <br/>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 </body>
 </html>
