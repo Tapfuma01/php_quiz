3541fbd748911e9af1c97aec7107b437e2f6a9b6
Tapfuma <enosmhundwa@gmail.com>
2019-08-20 19:21:55 +0200

    major updates on the css

--- index.php
+++ index.php
@@ -5,11 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/style.css">
-    <title>Document</title>
+     <!-- Bootstrap CSS -->
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+   
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <title>WILDLIFE QUIZ</title>
 </head>
-<body bgcolor='white'>
-<div class="hero-image">
-  <div class="hero-text">
+<body>
+
     <h1 style="font-size:35px">Enviromental QUIZ</h1>
     
   </div>
@@ -17,6 +25,7 @@
     <br/>
     <br/>
     <br/>
+    <div class="myquiz">
     <?php 
 
     $Questions = array(
@@ -29,6 +38,7 @@
             ),
             'CorrectAnswer' => 'B'
         ),
+         
         2 => array(
             'Question' => '2. What is the Capital of the Philippines',
             'Answers' => array(
@@ -253,7 +263,7 @@
     <?php 
     }
     ?>
-
+</div>
 
 
 
