94811aea8d1f852f95b6b6c1feacd5910a610115
Tapfuma <enosmhundwa@gmail.com>
2019-08-20 21:30:12 +0200

    update
    added bootstrap styling

--- index.php
+++ index.php
@@ -4,24 +4,90 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
      <!-- Bootstrap CSS -->
      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-   
+   <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <!-- Bootstrap core CSS -->
+    < link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.8/css/mdb.min.css" rel="stylesheet">
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <!-- JQuery -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.8/js/mdb.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <title>WILDLIFE QUIZ</title>
 </head>
 <body>
-    <div>
-    <h1  class="text-dark">Enviromental QUIZ</h1>
+   <!--Main Navigation-->
+<header>
+
+<nav class="navbar navbar-expand-lg navbar-dark black">
+    <div class="container">
+        <a class="navbar-brand" href="#"><strong>Tee Quizzes</strong></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Profile</a>
+                </li>
+            </ul>
+        </div>
     </div>
-    
-  </div>
+</nav>
+
+<div class="view intro-2">
+    <div class="full-bg-img">
+        <div class="mask rgba-black-strong flex-center">
+            <div class="container">
+                <div class="white-text text-center wow fadeInUp">
+                    <h2>Enviromental QUIZ </h2>
+                    <h5>Show your knowkedge about the environment</h5>
+                    <br>
+                    <p>SaVe ThE WoRLD</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+
+</header>
+<!--Main Navigation-->
+
+<!--Main Layout-->
+<main class="text-center my-5">
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+
+            <p align="justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+        </div>
+    </div>
+</div>
+
+</main>
+<!--Main Layout-->
     <br/>
     <br/>
     <br/>
@@ -49,7 +115,7 @@
             'CorrectAnswer' => 'C'
 		),
 		3 => array(
-            'Question' => '2. What is the Capital of the Philippines',
+            'Question' => '3. What is the Capital of the Philippines',
             'Answers' => array(
                 'A' => 'Cebu City',
                 'B' => 'Davao City',
@@ -58,7 +124,7 @@
             'CorrectAnswer' => 'C'
 		),
 		4 => array(
-            'Question' => '2. What is the Capital of the Philippines',
+            'Question' => '4. What is the Capital of the Philippines',
             'Answers' => array(
                 'A' => 'Cebu City',
                 'B' => 'Davao City',
@@ -67,7 +133,7 @@
             'CorrectAnswer' => 'C'
 		),
 		5 => array(
-            'Question' => '2. What is the Capital of the Philippines',
+            'Question' => '5. What is the Capital of the Philippines',
             'Answers' => array(
                 'A' => 'Cebu City',
                 'B' => 'Davao City',
@@ -76,7 +142,7 @@
             'CorrectAnswer' => 'C'
 		),
 		6=> array(
-            'Question' => '2. What is the Capital of the Philippines',
+            'Question' => '6. What is the Capital of the Philippines',
             'Answers' => array(
                 'A' => 'Cebu City',
                 'B' => 'Davao City',
@@ -85,7 +151,7 @@
             'CorrectAnswer' => 'C'
 		),
 		7 => array(
-            'Question' => '2. What is the Capital of the Philippines',
+            'Question' => '7. What is the Capital of the Philippines',
             'Answers' => array(
                 'A' => 'Cebu City',
                 'B' => 'Davao City',
@@ -94,7 +160,7 @@
             'CorrectAnswer' => 'C'
 		),
 		 8 => array(
-            'Question' => '2. What is the Capital of the Philippines',
+            'Question' => '8. What is the Capital of the Philippines',
             'Answers' => array(
                 'A' => 'Cebu City',
                 'B' => 'Davao City',
@@ -103,7 +169,7 @@
             'CorrectAnswer' => 'C'
 		),
 		9 => array(
-            'Question' => '2. What is the Capital of the Philippines',
+            'Question' => '9. What is the Capital of the Philippines',
             'Answers' => array(
                 'A' => 'Cebu City',
                 'B' => 'Davao City',
@@ -112,7 +178,7 @@
             'CorrectAnswer' => 'C'
 		),
 		10 => array(
-            'Question' => '2. What is the Capital of the Philippines',
+            'Question' => '10. What is the Capital of the Philippines',
             'Answers' => array(
                 'A' => 'Cebu City',
                 'B' => 'Davao City',
@@ -121,7 +187,7 @@
             'CorrectAnswer' => 'C'
 		),
 		11 => array(
-            'Question' => '2. What is the Capital of the Philippines',
+            'Question' => '11. What is the Capital of the Philippines',
             'Answers' => array(
                 'A' => 'Cebu City',
                 'B' => 'Davao City',
@@ -130,7 +196,7 @@
             'CorrectAnswer' => 'C'
 		),
 		12 => array(
-            'Question' => '2. What is the Capital of the Philippines',
+            'Question' => '12. What is the Capital of the Philippines',
             'Answers' => array(
                 'A' => 'Cebu City',
                 'B' => 'Davao City',
@@ -139,7 +205,7 @@
             'CorrectAnswer' => 'C'
 		),
 		13 => array(
-            'Question' => '2. What is the Capital of the Philippines',
+            'Question' => '13. What is the Capital of the Philippines',
             'Answers' => array(
                 'A' => 'Cebu City',
                 'B' => 'Davao City',
@@ -148,7 +214,7 @@
             'CorrectAnswer' => 'C'
 		),
 		14 => array(
-            'Question' => '2. What is the Capital of the Philippines',
+            'Question' => '14. What is the Capital of the Philippines',
             'Answers' => array(
                 'A' => 'Cebu City',
                 'B' => 'Davao City',
@@ -157,7 +223,7 @@
             'CorrectAnswer' => 'C'
 		),
 		15 => array(
-            'Question' => '2. What is the Capital of the Philippines',
+            'Question' => '15. What is the Capital of the Philippines',
             'Answers' => array(
                 'A' => 'Cebu City',
                 'B' => 'Davao City',
@@ -166,7 +232,7 @@
             'CorrectAnswer' => 'C'
 		),
 		16 => array(
-            'Question' => '2. What is the Capital of the Philippines',
+            'Question' => '16. What is the Capital of the Philippines',
             'Answers' => array(
                 'A' => 'Cebu City',
                 'B' => 'Davao City',
@@ -175,7 +241,7 @@
             'CorrectAnswer' => 'C'
 		),
 		17 => array(
-            'Question' => '2. What is the Capital of the Philippines',
+            'Question' => '17. What is the Capital of the Philippines',
             'Answers' => array(
                 'A' => 'Cebu City',
                 'B' => 'Davao City',
@@ -184,7 +250,7 @@
             'CorrectAnswer' => 'C'
 		),
 		18 => array(
-            'Question' => '2. What is the Capital of the Philippines',
+            'Question' => '18. What is the Capital of the Philippines',
             'Answers' => array(
                 'A' => 'Cebu City',
                 'B' => 'Davao City',
@@ -266,27 +332,7 @@
     <?php 
     }
     ?>
-
-
-
-
-
-
 </form>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 </body>
 </html>
