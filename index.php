138c513840b8c8003cb1cd6d81886f89d5010300
Tapfuma <enosmhundwa@gmail.com>
2019-08-20 18:27:50 +0200

    update on php and css files

--- index.php
+++ index.php
@@ -17,253 +17,243 @@
     <br/>
     <br/>
     <br/>
+    <?php 
 
-<form action="process.php" method="post">
-    <input type="submit">
-    
-<ol>
-     <li class= myborder>
-         <h3> What is my name?.<h3>
-             <div >
-             <input type="radio" name='firstAnswer' value='A' />
-             <label for='firstAnswerA'>a)Tanaka</label>
-            </div>
+    $Questions = array(
+        1 => array(
+            'Question' => '1. CSS stands for',
+            'Answers' => array(
+                'A' => 'Computer Styled Sections',
+                'B' => 'Cascading Style Sheets',
+                'C' => 'Crazy Solid Shapes'
+            ),
+            'CorrectAnswer' => 'B'
+        ),
+        2 => array(
+            'Question' => '2. What is the Capital of the Philippines',
+            'Answers' => array(
+                'A' => 'Cebu City',
+                'B' => 'Davao City',
+                'C' => 'Manila City'
+            ),
+            'CorrectAnswer' => 'C'
+		),
+		3 => array(
+            'Question' => '2. What is the Capital of the Philippines',
+            'Answers' => array(
+                'A' => 'Cebu City',
+                'B' => 'Davao City',
+                'C' => 'Manila City'
+            ),
+            'CorrectAnswer' => 'C'
+		),
+		4 => array(
+            'Question' => '2. What is the Capital of the Philippines',
+            'Answers' => array(
+                'A' => 'Cebu City',
+                'B' => 'Davao City',
+                'C' => 'Manila City'
+            ),
+            'CorrectAnswer' => 'C'
+		),
+		5 => array(
+            'Question' => '2. What is the Capital of the Philippines',
+            'Answers' => array(
+                'A' => 'Cebu City',
+                'B' => 'Davao City',
+                'C' => 'Manila City'
+            ),
+            'CorrectAnswer' => 'C'
+		),
+		6=> array(
+            'Question' => '2. What is the Capital of the Philippines',
+            'Answers' => array(
+                'A' => 'Cebu City',
+                'B' => 'Davao City',
+                'C' => 'Manila City'
+            ),
+            'CorrectAnswer' => 'C'
+		),
+		7 => array(
+            'Question' => '2. What is the Capital of the Philippines',
+            'Answers' => array(
+                'A' => 'Cebu City',
+                'B' => 'Davao City',
+                'C' => 'Manila City'
+            ),
+            'CorrectAnswer' => 'C'
+		),
+		 8 => array(
+            'Question' => '2. What is the Capital of the Philippines',
+            'Answers' => array(
+                'A' => 'Cebu City',
+                'B' => 'Davao City',
+                'C' => 'Manila City'
+            ),
+            'CorrectAnswer' => 'C'
+		),
+		9 => array(
+            'Question' => '2. What is the Capital of the Philippines',
+            'Answers' => array(
+                'A' => 'Cebu City',
+                'B' => 'Davao City',
+                'C' => 'Manila City'
+            ),
+            'CorrectAnswer' => 'C'
+		),
+		10 => array(
+            'Question' => '2. What is the Capital of the Philippines',
+            'Answers' => array(
+                'A' => 'Cebu City',
+                'B' => 'Davao City',
+                'C' => 'Manila City'
+            ),
+            'CorrectAnswer' => 'C'
+		),
+		11 => array(
+            'Question' => '2. What is the Capital of the Philippines',
+            'Answers' => array(
+                'A' => 'Cebu City',
+                'B' => 'Davao City',
+                'C' => 'Manila City'
+            ),
+            'CorrectAnswer' => 'C'
+		),
+		12 => array(
+            'Question' => '2. What is the Capital of the Philippines',
+            'Answers' => array(
+                'A' => 'Cebu City',
+                'B' => 'Davao City',
+                'C' => 'Manila City'
+            ),
+            'CorrectAnswer' => 'C'
+		),
+		13 => array(
+            'Question' => '2. What is the Capital of the Philippines',
+            'Answers' => array(
+                'A' => 'Cebu City',
+                'B' => 'Davao City',
+                'C' => 'Manila City'
+            ),
+            'CorrectAnswer' => 'C'
+		),
+		14 => array(
+            'Question' => '2. What is the Capital of the Philippines',
+            'Answers' => array(
+                'A' => 'Cebu City',
+                'B' => 'Davao City',
+                'C' => 'Manila City'
+            ),
+            'CorrectAnswer' => 'C'
+		),
+		15 => array(
+            'Question' => '2. What is the Capital of the Philippines',
+            'Answers' => array(
+                'A' => 'Cebu City',
+                'B' => 'Davao City',
+                'C' => 'Manila City'
+            ),
+            'CorrectAnswer' => 'C'
+		),
+		16 => array(
+            'Question' => '2. What is the Capital of the Philippines',
+            'Answers' => array(
+                'A' => 'Cebu City',
+                'B' => 'Davao City',
+                'C' => 'Manila City'
+            ),
+            'CorrectAnswer' => 'C'
+		),
+		17 => array(
+            'Question' => '2. What is the Capital of the Philippines',
+            'Answers' => array(
+                'A' => 'Cebu City',
+                'B' => 'Davao City',
+                'C' => 'Manila City'
+            ),
+            'CorrectAnswer' => 'C'
+		),
+		18 => array(
+            'Question' => '2. What is the Capital of the Philippines',
+            'Answers' => array(
+                'A' => 'Cebu City',
+                'B' => 'Davao City',
+                'C' => 'Manila City'
+            ),
+            'CorrectAnswer' => 'C'
+		),
+		19 => array(
+            'Question' => '19. What is the Capital of the Philippines',
+            'Answers' => array(
+                'A' => 'Cebu City',
+                'B' => 'Davao City',
+                'C' => 'Manila City'
+            ),
+            'CorrectAnswer' => 'C'
+		),
+		20 => array(
+            'Question' => '20. What is the Capital of the Philippines',
+            'Answers' => array(
+                'A' => 'Cebu City',
+                'B' => 'Davao City',
+                'C' => 'Manila City'
+            ),
+            'CorrectAnswer' => 'C'
+        )
+    );
 
+    if (isset($_POST['answers'])){
+        $Answers = $_POST['answers']; // Get submitted answers.
+
+        // Now this is fun, automated question checking! ;)
+
+        foreach ($Questions as $QuestionNo => $Value){
+            // Echo the question
+            echo $Value['Question'].'<br />';
+
+            if ($Answers[$QuestionNo] != $Value['CorrectAnswer']){
+                 echo 'You answered: <span style="color: red;">'.$Value['Answers'][$Answers[$QuestionNo]].'</span><br>'; // Replace style with a class
+                 echo 'Correct answer: <span style="color: green;">'.$Value['Answers'][$Value['CorrectAnswer']].'</span>';
+            } else {
+                echo 'Correct answer: <span style="color: green;">'.$Value['Answers'][$Answers[$QuestionNo]].'</span><br>'; // Replace style with a class
+                echo 'You are correct: <span style="color: green;">'.$Value['Answers'][$Answers[$QuestionNo]].'</span>'; $counter++;
+
+            }
+
+            echo '<br /><hr>'; 
+                                    if ($counter=="") 
+                                    { 
+                                    $counter='0';
+                                    $results = "Your score: $counter/2"; 
+                                    }
+                                    else 
+                                    { 
+                                    $results = "Your score: $counter/2"; 
+                                    }
+                }                           echo $results;
+    } else {  
+    ?>
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" id="quiz">
+        <?php foreach ($Questions as $QuestionNo => $Value){ ?>
+
+            <h3><?php echo $Value['Question']; ?></h3>
+            <?php 
+                foreach ($Value['Answers'] as $Letter => $Answer){ 
+                $Label = 'question-'.$QuestionNo.'-answers-'.$Letter;
+            ?>
             <div>
-             <input type="radio" name='firstAnswer' value='B' />
-             <label for='firstAnswerB'>b)Tawnda</label>
+                <input type="radio" name="answers[<?php echo $QuestionNo; ?>]" id="<?php echo $Label; ?>" value="<?php echo $Letter; ?>" />
+                <label for="<?php echo $Label; ?>"><?php echo $Letter; ?>) <?php echo $Answer; ?> </label>
             </div>
+            <?php } ?>
 
-            <div>
-             <input type="radio" name='firstAnswer' value='C' />
-             <label for='firstAnswerC'>c)Tapfuma</label>
-            </div>
+        <?php } ?>
+        <input type="submit" value="Submit Quiz" />
+        </form>
+    <?php 
+    }
+    ?>
 
-            <div>
-             <input type="radio" name='firstAnswer' value='D' />
-             <label for='firstAnswerD'>d)Tafara</label>
-            </div>
-                 
-
-    </li>
- 
-    <li>
-         <h3> What is my favorite music?.<h3>
-             <div>
-             <input type="radio" name='firstAnswer' value='A' />
-             <label for='firstAnswerA'>a)Pop</label>
-            </div>
-
-            <div>
-             <input type="radio" name='firstAnswer' value='B' />
-             <label for='firstAnswerB'>b)Dancehall</label>
-            </div>
-
-            <div>
-             <input type="radio" name='firstAnswer' value='C' />
-             <label for='firstAnswerC'>c)Country</label>
-            </div>
-
-            <div>
-             <input type="radio" name='firstAnswer' value='D' />
-             <label for='firstAnswerD'>d)All of the above</label>
-            </div>
-                 
-
-    </li>
-    <li>
-         <h3> What is my favorite <h3>
-             <div>
-             <input type="radio" name='firstAnswer' value='A' />
-             <label for='firstAnswerA'>a)Tanaka</label>
-            </div>
-
-            <div>
-             <input type="radio" name='firstAnswer' value='B' />
-             <label for='firstAnswerB'>b)Tawnda</label>
-            </div>
-
-            <div>
-             <input type="radio" name='firstAnswer' value='C' />
-             <label for='firstAnswerC'>c)Tapfuma</label>
-            </div>
-
-            <div>
-             <input type="radio" name='firstAnswer' value='D' />
-             <label for='firstAnswerD'>d)Tafara</label>
-            </div>
-                 
-
-    </li>
-    <li>
-         <h3> What is my name<h3>
-             <div>
-             <input type="radio" name='firstAnswer' value='A' />
-             <label for='firstAnswerA'>a)Tanaka</label>
-            </div>
-
-            <div>
-             <input type="radio" name='firstAnswer' value='B' />
-             <label for='firstAnswerB'>b)Tawnda</label>
-            </div>
-
-            <div>
-             <input type="radio" name='firstAnswer' value='C' />
-             <label for='firstAnswerC'>c)Tapfuma</label>
-            </div>
-
-            <div>
-             <input type="radio" name='firstAnswer' value='D' />
-             <label for='firstAnswerD'>d)Tafara</label>
-            </div>
-                 
-
-    </li>
-    <li>
-         <h3> What is my name<h3>
-             <div>
-             <input type="radio" name='firstAnswer' value='A' />
-             <label for='firstAnswerA'>a)Tanaka</label>
-            </div>
-
-            <div>
-             <input type="radio" name='firstAnswer' value='B' />
-             <label for='firstAnswerB'>b)Tawnda</label>
-            </div>
-
-            <div>
-             <input type="radio" name='firstAnswer' value='C' />
-             <label for='firstAnswerC'>c)Tapfuma</label>
-            </div>
-
-            <div>
-             <input type="radio" name='firstAnswer' value='D' />
-             <label for='firstAnswerD'>d)Tafara</label>
-            </div>
-                 
-
-    </li>
-    <li>
-         <h3> What is my name<h3>
-             <div>
-             <input type="radio" name='firstAnswer' value='A' />
-             <label for='firstAnswerA'>a)Tanaka</label>
-            </div>
-
-            <div>
-             <input type="radio" name='firstAnswer' value='B' />
-             <label for='firstAnswerB'>b)Tawnda</label>
-            </div>
-
-            <div>
-             <input type="radio" name='firstAnswer' value='C' />
-             <label for='firstAnswerC'>c)Tapfuma</label>
-            </div>
-
-            <div>
-             <input type="radio" name='firstAnswer' value='D' />
-             <label for='firstAnswerD'>d)Tafara</label>
-            </div>
-                 
-
-    </li>
-    <li>
-         <h3> What is my name<h3>
-             <div>
-             <input type="radio" name='firstAnswer' value='A' />
-             <label for='firstAnswerA'>a)Tanaka</label>
-            </div>
-
-            <div>
-             <input type="radio" name='firstAnswer' value='B' />
-             <label for='firstAnswerB'>b)Tawnda</label>
-            </div>
-
-            <div>
-             <input type="radio" name='firstAnswer' value='C' />
-             <label for='firstAnswerC'>c)Tapfuma</label>
-            </div>
-
-            <div>
-             <input type="radio" name='firstAnswer' value='D' />
-             <label for='firstAnswerD'>d)Tafara</label>
-            </div>
-                 
-
-    </li>
-    <li>
-         <h3> What is my name<h3>
-             <div>
-             <input type="radio" name='firstAnswer' value='A' />
-             <label for='firstAnswerA'>a)Tanaka</label>
-            </div>
-
-            <div>
-             <input type="radio" name='firstAnswer' value='B' />
-             <label for='firstAnswerB'>b)Tawnda</label>
-            </div>
-
-            <div>
-             <input type="radio" name='firstAnswer' value='C' />
-             <label for='firstAnswerC'>c)Tapfuma</label>
-            </div>
-
-            <div>
-             <input type="radio" name='firstAnswer' value='D' />
-             <label for='firstAnswerD'>d)Tafara</label>
-            </div>
-                 
-
-    </li>
-    <li>
-         <h3> What is my name<h3>
-             <div>
-             <input type="radio" name='firstAnswer' value='A' />
-             <label for='firstAnswerA'>a)Tanaka</label>
-            </div>
-
-            <div>
-             <input type="radio" name='firstAnswer' value='B' />
-             <label for='firstAnswerB'>b)Tawnda</label>
-            </div>
-
-            <div>
-             <input type="radio" name='firstAnswer' value='C' />
-             <label for='firstAnswerC'>c)Tapfuma</label>
-            </div>
-
-            <div>
-             <input type="radio" name='firstAnswer' value='D' />
-             <label for='firstAnswerD'>d)Tafara</label>
-            </div>
-                 
-
-    </li>
-    <li>
-         <h3> What is my name<h3>
-             <div>
-             <input type="radio" name='firstAnswer' value='A' />
-             <label for='firstAnswerA'>a)Tanaka</label>
-            </div>
-
-            <div>
-             <input type="radio" name='firstAnswer' value='B' />
-             <label for='firstAnswerB'>b)Tawnda</label>
-            </div>
-
-            <div>
-             <input type="radio" name='firstAnswer' value='C' />
-             <label for='firstAnswerC'>c)Tapfuma</label>
-            </div>
-
-            <div>
-             <input type="radio" name='firstAnswer' value='D' />
-             <label for='firstAnswerD'>d)Tafara</label>
-            </div>
-                 
-
-    </li>
-</ol>
 
 
 
